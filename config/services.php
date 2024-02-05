9340f7da5b3580e33a764a36d2de2b053a9bb985
Simi oluwatomi <deifilius1@gmail.com>
2024-02-06 00:08:37 +0100

    chore: update app config

--- config/services.php
+++ config/services.php
@@ -14,11 +14,34 @@ return [
     |
     */
 
+    'lemon-squeezy' => [
+        'secret_key' => env('LEMONSQUEEZY_SECRET_KEY',),
+        'webhook' => [
+            'secret_key' => env('LEMONSQUEEZY_WEBHOOK_SECRET_KEY'),
+            'events' => explode(',', env('LEMONSQUEEZY_WEBHOOK_EVENTS', sprintf(
+                '%s',
+                'order_created,order_refunded,subscription_created,subscription_updated',
+            ))),
+        ]
+    ],
+
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
         'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
         'scheme' => 'https',
+    ],
+
+    'paystack' => [
+        'secret_key' => env('PAYSTACK_SECRET_KEY',),
+        'public_key' => env('PAYSTACK_PUBLIC_KEY',),
+        'webhook' => [
+            'source_ips' => ['52.31.139.75', '52.49.173.169', '52.214.14.220'],
+            'events' => explode(',', env('PAYSTACK_WEBHOOK_EVENTS', sprintf(
+                '%s',
+                'charge.success,transfer.failed,transfer.success,transfer.reversed',
+            ))),
+        ]
     ],
 
     'postmark' => [
