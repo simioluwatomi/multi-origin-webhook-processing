562567ad2e7a0a1f0948ece2ad9ee7e8135ca2f4
Simi oluwatomi <deifilius1@gmail.com>
2024-02-06 00:09:25 +0100

    feat: store webhooks

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -2,12 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\WebhookException;
+use App\Jobs\ProcessWebhookJob;
+use App\Models\Webhook;
+use App\Support\Managers\WebhookManager;
+use App\ThirdParty\WebhookProcessors\Contracts\WebhookProcessorInterface;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Support\Str;
 
 class WebhookController extends Controller
 {
-    public function store(Request $request, string $origin)
+    /**
+     * @throws \Throwable
+     */
+    public function store(Request $request, string $origin, WebhookManager $manager): JsonResponse
     {
-        // TODO implement webhook processing
+        /** @var WebhookProcessorInterface|null $webhookProcessor */
+        $webhookProcessor = $manager->driver(Str::lower($origin));
+
+        throw_if(
+            $webhookProcessor === null,
+            new WebhookException(message: "Could not find a webhook processor for the origin: {$origin}")
+        );
+
+        $webhookProcessor->validate($request);
+
+        $webhook = Webhook::create([
+            'origin' => $webhookProcessor->getName(),
+            'event_name' => $webhookProcessor->getEventName($request->input()),
+            'payload' => $request->input(),
+        ]);
+
+        Queue::push(new ProcessWebhookJob($webhook));
+
+        if ($webhookProcessor->originExpectsAResponse()) {
+            return response()->json(data: $webhookProcessor->generateResponse($webhook));
+        }
+
+        return response()->json();
     }
 }
